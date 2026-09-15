1cc4c4530e5d2c6aa4b8a63e7ed6b91ff9de9678
Lakshitha <slmart15695@gmail.com>
2026-09-14 18:57:05 -0700

    Update index.js with Matchmaking Room logic and remove server.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -11,26 +14,41 @@ const io = new Server(server, {
     }
 });
 
-let onlinePlayers = 0;
+let waitingPlayer = null;
 
 io.on('connection', (socket) => {
-    onlinePlayers++;
-    console.log(`User connected: ${socket.id} | Total online: ${onlinePlayers}`);
-    
-    io.emit('online-players', onlinePlayers);
+    console.log('Player connected:', socket.id);
 
-    socket.on('player-move', (moveData) => {
-        socket.broadcast.emit('opponent-move', moveData);
+    // Players 2 Matchmaking Logic
+    if (waitingPlayer) {
+        const roomId = `room_${waitingPlayer.id}_${socket.id}`;
+        socket.join(roomId);
+        waitingPlayer.join(roomId);
+
+        // Send role and roomId to players
+        waitingPlayer.emit('gameStart', { color: 'red', roomId });
+        socket.emit('gameStart', { color: 'white', roomId });
+
+        waitingPlayer = null;
+    } else {
+        waitingPlayer = socket;
+        socket.emit('waiting', 'Waiting for an opponent...');
+    }
+
+    // Pass moves between players
+    socket.on('makeMove', (data) => {
+        socket.to(data.roomId).emit('moveMade', data);
     });
 
     socket.on('disconnect', () => {
-        onlinePlayers--;
-        console.log(`User disconnected: ${socket.id} | Total online: ${onlinePlayers}`);
-        io.emit('online-players', onlinePlayers);
+        console.log('Player disconnected:', socket.id);
+        if (waitingPlayer && waitingPlayer.id === socket.id) {
+            waitingPlayer = null;
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`CheckerX Backend Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
